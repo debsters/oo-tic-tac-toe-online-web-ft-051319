2db473dca26ac3725a87c5628bd1979db8543fe0
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-20 00:02:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,4 +52,15 @@ class TicTacToe
     end
   end
 
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      position_1 = @board[win_combo[0]]
+      position_2 = @board[win_combo[1]]
+      position_3 = @board[win_combo[2]]
+
+       position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+      position_1 == "O" && position_2 == "O" && position_3 == "O" 
+    end
+  end
+  
 end
